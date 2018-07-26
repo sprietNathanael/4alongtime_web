3a4d75c9d7350c112a06a2ca0b893f59ec14de79
Nathanaël Spriet <nathanael.spriet@gmail.com>
2018-07-26 17:18:15 +0200

    Adds some logo and make the smooth scroll

--- src/js/main.js
+++ src/js/main.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
     createCharts();
 });
 
-$( window ).on('load',function(){
+$(window).on('load', function () {
     scrollToTop();
     setInterval(refreshCounter, 500);
     refreshCounter();
@@ -21,23 +21,59 @@ $( window ).on('load',function(){
 
 // When the user clicks on the button, scroll to the top of the document
 function scrollToTop() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
+    // document.body.scrollTop = 0;
+    // document.documentElement.scrollTop = 0;
+    smoothScroll(0, "#");
 }
 
-function refreshCounter(){
+function refreshCounter() {
     var now = new Date();
     var diff = endDate - now;
-    days = Math.floor(diff/86400000);
-    rest = Math.floor(diff%86400000);
-    hours = Math.floor(rest/3600000);
-    rest = Math.floor(rest%3600000);
-    min = Math.floor(rest/60000);
-    rest = Math.floor(rest%60000);
-    sec = Math.floor(rest/1000);
-    $("#counter_d").html(""+days);
-    $("#counter_h").html((hours < 10 ? "0" : "")+hours);
-    $("#counter_m").html((min < 10 ? "0" : "")+min);
-    $("#counter_s").html((sec < 10 ? "0" : "")+sec);
+    days = Math.floor(diff / 86400000);
+    rest = Math.floor(diff % 86400000);
+    hours = Math.floor(rest / 3600000);
+    rest = Math.floor(rest % 3600000);
+    min = Math.floor(rest / 60000);
+    rest = Math.floor(rest % 60000);
+    sec = Math.floor(rest / 1000);
+    $("#counter_d").html("" + days);
+    $("#counter_h").html((hours < 10 ? "0" : "") + hours);
+    $("#counter_m").html((min < 10 ? "0" : "") + min);
+    $("#counter_s").html((sec < 10 ? "0" : "") + sec);
 
+}
+
+// Add scrollspy to <body>
+$('body').scrollspy({
+    target: ".navbar",
+    offset: 50
+});
+
+// Add smooth scrolling on all links inside the navbar
+$(".smoothScroll a").on('click', function (event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+
+        // Prevent default anchor click behavior
+        event.preventDefault();
+
+        // Store hash
+        var hash = this.hash;
+        smoothScroll($(hash).offset().top, hash);
+
+    } // End if
+
+});
+
+function smoothScroll(top, hash) {
+    // Using jQuery's animate() method to add smooth page scroll
+    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+    $('html, body').animate({
+        scrollTop: top
+    }, 800, function () {
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+    });
 }
